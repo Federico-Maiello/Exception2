26893cbd8020c00e40db1371c8e6a80df7608979
Federico Maiello <federicomaiello@hotmail.it>
2023-11-16 14:10:39 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
-    public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws NotANumberException {
+        char character = 'f';
+        checkIfNumber(character);
+    }
+
+    public static void checkIfNumber(char character) throws NotANumberException {
+        if (Character.isDigit(character)) {
+
+            System.out.println("The char is number.");
+        } else {
+
+            throw new NotANumberException("The char isn't number.");
+        }
+    }
+}
+
+class NotANumberException extends Exception {
+    public NotANumberException(String message) {
+        super(message);
     }
 }
